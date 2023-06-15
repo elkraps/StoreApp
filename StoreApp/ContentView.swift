17e01526e3846737b1954386c6fbf120e1e430c9
elkraps <dobroisreal@mail.ru>
2023-06-15 18:26:43 +0300

    Creating new project.

--- StoreApp/ContentView.swift
+++ StoreApp/ContentView.swift
@@ -8,19 +8,160 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedIndex: Int = 0
+    private let categories = ["All","Shoes","Bags","Glasses","Accessories","T-Shirts","Hoodies"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(hex: "D3D3D3")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack(alignment: .leading) {
+                AppBarView()
+                
+                SearchBar()
+                
+                SaleView()
+                
+                Text("NEW IN")
+                    .font(.system(size: 25, weight: .bold))
+                    .padding(.leading)
+                    .padding(.bottom, -15)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0 ..< categories.count) { item in
+                            CategoryView(isActive: item == selectedIndex, text: categories[item])
+                                .onTapGesture {
+                                    selectedIndex = item
+                                }
+                        }
+                    }
+                }
+                .padding()
+                
+                
+                
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image("hamburger")
+                    .padding(.all, 12)
+                    .background(.white)
+                    .cornerRadius(25)
+            }
+            
+            Spacer()
+            
+            Text("Awesome")
+                .font(.system(size: 25, weight: .bold))
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image("bell")
+                    .padding(.all, 12)
+                    .background(.white)
+                    .cornerRadius(25)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct SearchBar: View {
+    @State private var search: String = ""
+    var body: some View {
+        HStack {
+            Image("search")
+            TextField("Search", text: $search)
+            Button {
+                
+            } label: {
+                Image(systemName: "mic")
+                    .foregroundColor(.black)
+            }
+            
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(25)
+        .padding(.all, 15)
+    }
+}
+
+struct ImageOverlay: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Get your \nspecial sale")
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(.white)
+            
+            Button {
+                
+            } label: {
+                Text("Up to 65% ")
+                    .padding(.all, 5)
+                    .font(.system(size: 15))
+                    .background(.white)
+                    .foregroundColor(.black)
+                    
+            }
+            .cornerRadius(10)
+            .opacity(0.9)
+        }
+        .padding(.leading, 30)
+    }
+}
+
+struct SaleView: View {
+    var body: some View {
+        HStack {
+            Image("sneek")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .infinity, height: 150)
+                .cornerRadius(25)
+                .padding()
+                .overlay(ImageOverlay(), alignment: .leading)
+            
+        }
+        .padding(.top, -15)
+    }
+}
+
+struct CategoryView: View {
+    let isActive: Bool
+    let text: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(text)
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(isActive ? Color(.black) : Color.black.opacity(0.5))
+            
+            if (isActive) {
+                Color(.black)
+                    .frame(width: 15, height: 2)
+                    .clipShape(Capsule())
+            }
+        }
+
     }
 }
