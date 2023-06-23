f040ba51f161a5cae05bbe53e04a3a054ed3eb32
elkraps <dobroisreal@mail.ru>
2023-06-23 22:09:07 +0300

    Profile view.

--- StoreApp/Screens/ProfileView.swift
+++ StoreApp/Screens/ProfileView.swift
@@ -8,51 +8,142 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
     var body: some View {
         ZStack {
             Color(hex: "D3D3D3")
                 .edgesIgnoringSafeArea(.all)
             
             ScrollView {
+                ProfileBar()
+                
+                AsapView()
+                
+                StatView()
+                
+                
                 VStack(alignment: .leading) {
-                    HStack {
-                        Button {
-                            //
-                        } label: {
-                            Image(systemName: "ellipsis")
-                                .padding(.all, 12)
-                                .background(.white)
-                                .foregroundColor(.black)
-                                .cornerRadius(30)
-                        }
+                    Text("Personal Information")
+                    
+                    
+                    VStack {
                         
-                        Spacer()
-                        
-                        Text("Profile")
-                        
-                        Spacer()
-                        
-                        Button {
-                            //
-                        } label: {
-                            Image(systemName: "ellipsis")
-                                .padding(.all, 12)
-                                .background(.white)
-                                .foregroundColor(.black)
-                                .cornerRadius(25)
-                        }
                     }
-                    .padding()
                 }
+                
             }
         }
-        
-        
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+struct ProfileBar: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Spacer()
+                
+                Text("Profile")
+                    .font(.system(size: 25, weight: .bold))
+                    .padding(.leading, 40)
+                
+                Spacer()
+                
+                Button {
+                    //
+                } label: {
+                    Image("ellipsis")
+                        .padding(.all, 8)
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(25)
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct AsapView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            Image("Profile")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 200)
+                .clipShape(Circle())
+            
+            Text("Rakim Athelston Mayers")
+                .font(.system(size: 30, weight: .semibold))
+            
+            Text(verbatim: "asapRocky@gmail.bombom")
+                .foregroundColor(.black)
+                .font(.system(size: 18, weight: .light))
+                .opacity(0.9)
+            
+        }
+        .padding(.top, -15)
+    }
+}
+
+struct StatView: View {
+    var body: some View {
+        VStack{
+            HStack{
+                
+                VStack {
+                    Image(systemName: "handbag")
+                        .frame(width: 45, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(.black)
+                        .clipShape(Circle())
+                    
+                    Text("Progress order")
+                        .font(.system(size: 15))
+                    Text("15+")
+                }
+                
+                
+                VStack {
+                    Image(systemName: "ticket")
+                        .frame(width: 45, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(.black)
+                        .clipShape(Circle())
+                    
+                    Text("Promocodes")
+                        .font(.system(size: 15))
+                    Text("8")
+                }
+                .padding(.trailing, 20)
+                
+                
+                
+                VStack {
+                    Image(systemName: "star")
+                        .frame(width: 45, height: 45)
+                        .foregroundColor(Color.white)
+                        .background(.black)
+                        .clipShape(Circle())
+                    
+                    Text("Reviews")
+                        .font(.system(size: 15))
+                    Text("5.5K")
+                }
+                .padding(.trailing, 20)
+            }
+            
+            .padding()
+            
+        }
+        .padding(.all, 15)
+        .background(Color.white)
+        .cornerRadius(25)
+        .padding(.top, 10)
     }
 }
